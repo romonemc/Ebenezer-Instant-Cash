c5fe15a491b89ea59bde10a41d287f1409112aef
Romone McFarlane <romonemc@gmail.com>
2018-03-15 21:44:40 -0500

    Revert "Created Report and Main Form"
    This reverts commit 5997f9e551290bca5b6481ac9f118b8139347bed.

--- Ebenezer Instant Cash/Program.cs
+++ Ebenezer Instant Cash/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace Ebenezar
+namespace WindowsFormsApp1
 {
     static class Program
     {
@@ -15,7 +15,7 @@ namespace Ebenezar
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new Form1());
         }
     }
 }
